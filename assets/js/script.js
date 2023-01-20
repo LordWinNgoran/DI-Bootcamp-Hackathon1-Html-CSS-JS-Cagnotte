d5a47421f6758a2ac8b52bad2ec3b63062eb0430
LordWinNgoran <cyrille4011@gmail.com>
2023-01-20 16:22:09 +0100

    game logic and test.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,402 +1,10 @@
-let arrayQuestion=[
-    {
-        id:1,
-        descript_question:'How long is a marathon?',
-        trueResponse:'42.195 kilometres',
-        category:'Sport',
-        anotherResponse1:'4 kilometres',
-        anotherResponse2:'12.9 kilometres',
-        anotherResponse3:'22.5 kilometres',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:2,
-        descript_question:'How many players are there on a baseball team?',
-        trueResponse:'9 players',
-        category:'Sport',
-        anotherResponse1:'11 players',
-        anotherResponse2:'14 players',
-        anotherResponse3:'8 players',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:3,
-        descript_question:'Which country won the World Cup 2018?',
-        trueResponse:'France',
-        category:'Sport',
-        anotherResponse1:'Argentina',
-        anotherResponse2:'Italy',
-        anotherResponse3:'United State',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:4,
-        descript_question:'What sport is considered the “king of sports”?',
-        trueResponse:'Soccer',
-        category:'Sport',
-        anotherResponse1:'BaseBall',
-        anotherResponse2:'Basket Ball',
-        anotherResponse3:'Tennis',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:5,
-        descript_question:'What team won the first NBA game in 1946?',
-        trueResponse:'The New York Knicks',
-        category:'Sport',
-        anotherResponse1:'The Brooklyn Nets',
-        anotherResponse2:'The Chicago Bulls',
-        points:7,
-        bonus:0,
-
-    },
-    {
-        id:6,
-        descript_question:'In which sport would you have a touchdown?',
-        trueResponse:'American football',
-        category:'Sport',
-        anotherResponse1:'Football',
-        anotherResponse2:'BaseBall',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:7,
-        descript_question:'How many gold medals has Usain Bolt won?',
-        trueResponse:'8',
-        category:'Sport',
-        anotherResponse1:'4',
-        anotherResponse2:'7',
-        anotherResponse3:'6',
-        points:6,
-        bonus:0,
-
-    },
-    {
-        id:8,
-        descript_question:'Which female tennis player has won the highest number of Grand Slams?',
-        trueResponse:'Margaret Court (24)',
-        category:'Sport',
-        anotherResponse1:'Chris Evert(25)',
-        anotherResponse2:'Martina Navratilova(32)',
-        anotherResponse3:'Helen Wills Moody(17)',
-        points:7,
-        bonus:0,
-
-    },
-    {
-        id:9,
-        descript_question:'Which footballer has scored the most goals in World Cup history?',
-        trueResponse:'Miroslav Klose',
-        category:'Sport',
-        anotherResponse1:'Ronaldo Nazario',
-        anotherResponse2:'Gerd Müller',
-        anotherResponse3:'Lionel Messi',
-        points:6,
-        bonus:0,
-
-    },
-
-
-    {
-        id:10,
-        descript_question:'Who were the joint top scorers in the 2021/22 Premier League season?',
-        trueResponse:'Margaret Court (24)',
-        category:'Sport',
-        anotherResponse1:'Harry Kane and Sadio Mane (with 24 goals)',
-        anotherResponse2:'Wilfrid Zaha and Gilberto Martinez (with 23 goals)',
-        points:6,
-        bonus:0,
-
-    },
-    {
-        id:11,
-        descript_question:'Where is the next Summer Olympic Games set to take place?',
-        trueResponse:'Paris',
-        category:'Sport',
-        anotherResponse1:'Milan',
-        anotherResponse2:'Londre',
-        anotherResponse3:'Abidjan',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:12,
-        descript_question:'Who has won more grand slam tennis titles?',
-        trueResponse:'Roger Federer',
-        category:'Sport',
-        anotherResponse1:'Serena Williams',
-        points:5,
-        bonus:0,
-
-    },
-
-
-
-
-
-
-
-
-
-
-
-    {
-        id:13,
-        descript_question:'What is the capital of Finland?',
-        trueResponse:'Helsinki',
-        category:'General knowledge',
-        anotherResponse1:'Yerevan',
-        anotherResponse2:'Vienna',
-        anotherResponse3:'Minsk',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:14,
-        descript_question:'Which five colours make up the Olympic rings?',
-        trueResponse:'Black, green, blue, yellow and red',
-        category:'General knowledge',
-        anotherResponse1:'Black, white, blue, yellow and red',
-        anotherResponse2:'Black, brown, blue and red',
-        anotherResponse3:'Green, Black, blue, yellow and red',
-        points:6,
-        bonus:0,
-
-    },
-    {
-        id:15,
-        descript_question:'What\'s the biggest animal in the world?',
-        trueResponse:'The blue whale',
-        category:'General knowledge',
-        anotherResponse1:'Elephant',
-        anotherResponse2:'Whale',
-        anotherResponse3:'Whale Shark',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:16,
-        descript_question:'Which planet is closest to the sun?',
-        trueResponse:'Mercury',
-        category:'General knowledge',
-        anotherResponse1:'Jupiter',
-        anotherResponse2:'Venus',
-        anotherResponse3:'Mars',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:17,
-        descript_question:'What is the largest country in the world',
-        trueResponse:'Russia',
-        category:'General knowledge',
-        anotherResponse1:'Albania',
-        anotherResponse2:'Togo',
-        anotherResponse3:'Australia',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:18,
-        descript_question:'How many valves does the heart have?',
-        trueResponse:'Four heart valves',
-        category:'General knowledge',
-        anotherResponse1:'Five heart valves',
-        anotherResponse2:'Six heart valves',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:19,
-        descript_question:'What is the capital of Portugal?',
-        trueResponse:'Lisbon',
-        category:'General knowledge',
-        anotherResponse1:'Buenos Aires',
-        anotherResponse2:'Vienna',
-        anotherResponse3:'Canberra',
-        points:5,
-        bonus:0,
-    },
-    {
-        id:20,
-        descript_question:'The end of the First World War?',
-        trueResponse:'1918',
-        category:'General knowledge',
-        anotherResponse1:'1928',
-        anotherResponse2:'1938',
-        anotherResponse3:'1921',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:21,
-        descript_question:'Martin Luther launches the Reformation?',
-        trueResponse:'1517',
-        category:'General knowledge',
-        anotherResponse1:'1519',
-        anotherResponse2:'1525',
-        anotherResponse3:'1535',
-        points:7,
-        bonus:0,
-
-    },
-
-
-    {
-        id:22,
-        descript_question:'Where in the world is Leonardo da Vinci’s Mona Lisa exhibited?',
-        trueResponse:'The Louvre, Paris, France',
-        category:'General knowledge',
-        anotherResponse1:'The Louvre, Lyon, France',
-        anotherResponse2:'The Louvre, île De France, France',
-        points:7,
-        bonus:0,
-
-    },
-    {
-        id:23,
-        descript_question:'What nationality was sculptor Alberto Giacometti?',
-        trueResponse:'Swiss',
-        category:'General knowledge',
-        anotherResponse1:'Jamaican',
-        anotherResponse2:'Colombian',
-        anotherResponse3:'Moldovan',
-        points:7,
-        bonus:0,
-
-    },
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    {
-        id:24,
-        descript_question:'What colour is the Mangosteen?',
-        trueResponse:'purplish red',
-        category:'Food & fruit',
-        anotherResponse1:'purplish blue',
-        anotherResponse2:'purplish yellow',
-        anotherResponse3:'purplish green',
-        points:6,
-        bonus:0,
-
-    },
-    {
-        id:25,
-        descript_question:'Where does the Rambutan come from?',
-        trueResponse:'Malaysia',
-        category:'Food & fruit',
-        anotherResponse1:'China',
-        anotherResponse2:'Japan',
-        points:6,
-        bonus:0,
-
-    },
-    {
-        id:26,
-        descript_question:'What country is the largest producer of kiwi fruit?',
-        trueResponse:'Italy',
-        category:'Food & fruit',
-        anotherResponse1:'Chile',
-        anotherResponse2:'Greece',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:27,
-        descript_question:'What is the most consumed fruit in the world?',
-        trueResponse:'Banana',
-        category:'Food & fruit',
-        anotherResponse1:'Apple',
-        anotherResponse2:'Orange',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:28,
-        descript_question:'How much water is there in a melon?',
-        trueResponse:'98%',
-        category:'Food & fruit',
-        anotherResponse1:'95%',
-        anotherResponse2:'90%',
-        anotherResponse3:'88%',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:29,
-        descript_question:'What is the highest peak in Africa?',
-        trueResponse:'Mount Kilimanjaro',
-        category:'General knowledge',
-        anotherResponse1:'Mount Nimba',
-        anotherResponse2:'Mount Tonkpi',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:30,
-        descript_question:'Which mountain range stretches across Algeria and Morocco?',
-        trueResponse:'Atlas Mountains',
-        category:'General knowledge',
-        anotherResponse1:'Majra Mountains',
-        anotherResponse2:'Athena Mountains',
-        points:5,
-        bonus:0,
-
-    },
-    {
-        id:32,
-        descript_question:'Which state in Africa is most populous?',
-        trueResponse:'Nigeria',
-        category:'General knowledge',
-        anotherResponse1:'Ivory Coast',
-        anotherResponse2:'Ghana',
-        points:5,
-        bonus:0,
-
-    },
-    
-
-
-
-    
-
-]
-
+let userName = sessionStorage.getItem('auth');
+
+if(localStorage.getItem(userName)){
+    alert('ok')
+}else{
+    alert('non ok')
+}
 
 let number=prompt('Please give us your favorite number')
 number=parseInt(number)
@@ -421,18 +29,28 @@ document.querySelector('div').appendChild(createButton) */
 console.log('random',rand,'calcul',calcul)
 console.log("The rand"+rand)
 
+
 let button=document.querySelector('button')
+
+
 
 button.addEventListener('click',(e)=>{
     e.preventDefault()
     let input = document.querySelector('input').value
-    if(input==calcul){
+    
+    let stockUserinfo=JSON.parse(localStorage.getItem("cyrillengoran"))
+      
 
+    if(input==calcul){
         alert("Bravo vous avez trouvé en 50 secondes vous avez 2 chances ")
-        localStorage.setItem('nbrChances',JSON.stringify(userobj))
+        stockUserinfo.nombreChance=2
+        console.log(stockUserinfo.password)
         document.body.remove(document.getElementById('register'))
     }else{
         alert("Désolé vous n'avez pas trouvé mais vous avez une chance de remporter un prix ")
+        stockUserinfo.nombreChance=1
+        console.log(stockUserinfo.nombreChance)
+        document.body.remove(document.getElementById('register'))
     }
     console.log("Voici le number "+ input)
 })
@@ -441,8 +59,15 @@ button.addEventListener('click',(e)=>{
 
 
 
-
-
-
-
 console.log(arrayQuestion)
+
+
+
+    //    let userobj={
+    //         username : usernamevalue,
+    //         mail : email,
+    //         password : btoa(password)
+    //     }
+    //     localStorage.setItem('userinfo',JSON.stringify(userobj))
+    //     return false;
+
